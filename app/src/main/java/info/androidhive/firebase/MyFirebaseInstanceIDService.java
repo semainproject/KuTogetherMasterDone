5b1c1629e282a31536f01b3f2d343518412d91c0
KUTogetherV2 <31027365+semainproject@users.noreply.github.com>
2017-10-09 22:36:26 +0700

    จากกันย์เอง

--- app/src/main/java/info/androidhive/firebase/MyFirebaseInstanceIDService.java
+++ app/src/main/java/info/androidhive/firebase/MyFirebaseInstanceIDService.java
@@ -20,18 +20,30 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
     private static final String TAG = "MyFirebaseIIDService";
     DatabaseReference dbConnect;
+    public String currrentToken;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        currrentToken = FirebaseInstanceId.getInstance().getToken();
+        //currentToken = FirebaseInstanceId.getInstance().getToken();
+    }
+
     /**
      * Called if InstanceID token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the InstanceID token
      * is initially generated so this is where you would retrieve the token.
      */
+
     @Override
     public void onTokenRefresh() {
         super.onTokenRefresh();
 
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
-        sendRegistrationToServer(refreshedToken);
+        if(refreshedToken!=null) {
+            sendRegistrationToServer(refreshedToken);
+        }
     }
 
 
@@ -47,16 +59,20 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user.getUid();
         dbConnect = FirebaseDatabase.getInstance().getReference("USER").child(uid);
-        dbConnect.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                dbConnect.child("FCMTOKEN").setValue(token);
-            }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+        if(!currrentToken.equals(token)) {
+            dbConnect.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                        dbConnect.child("FCMTOKEN").setValue(token);
 
-            }
-        });
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 }
