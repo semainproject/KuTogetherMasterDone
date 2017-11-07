fb331341633cc66f012b4eb18b032d46e110d465
Kittaporn <kittaporn.j@ku.th>
2017-11-07 16:30:58 +0700

    before se class

--- app/src/main/java/info/androidhive/firebase/editProfile.java
+++ app/src/main/java/info/androidhive/firebase/editProfile.java
@@ -23,6 +23,12 @@ public class editProfile extends AppCompatActivity {
     EditText lastname;
     EditText studentID;
     Button submit;
+    CheckBox Passenger_box;
+    CheckBox Driver_box;
+    EditText BikeID;
+    EditText brand;
+    EditText color;
+
     String uid,change_name,change_lastname,change_id;
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
@@ -41,6 +47,12 @@ public class editProfile extends AppCompatActivity {
         name = (EditText) findViewById(R.id.nameTx);
         female = (CheckBox) findViewById(R.id.female);
         male = (CheckBox) findViewById(R.id.male);
+        Passenger_box = (CheckBox) findViewById(R.id.Passenger_box);
+        Driver_box = (CheckBox) findViewById(R.id.Driver_box);
+        BikeID = (EditText) findViewById(R.id.BikeID);
+        brand = (EditText) findViewById(R.id.brand);
+        color = (EditText) findViewById(R.id.color);
+
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         uid = user.getUid();
         infoDB = FirebaseDatabase.getInstance().getReference("USER").child(uid).child("INFORMATION");
@@ -59,6 +71,20 @@ public class editProfile extends AppCompatActivity {
                 }
                 else{
                     female.setChecked(true);
+                }
+
+                if(infoUser.getTypePassDriv().equals("Passenger")){
+                    Passenger_box.setChecked(true);
+                    BikeID.setVisibility(View.INVISIBLE);
+                    brand.setVisibility(View.INVISIBLE);
+                    color.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    Driver_box.setChecked(true);
+                    Passenger_box.setChecked(false);
+                    BikeID.setVisibility(View.VISIBLE);
+                    brand.setVisibility(View.VISIBLE);
+                    color.setVisibility(View.VISIBLE);
                 }
                 submit.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -81,7 +107,26 @@ public class editProfile extends AppCompatActivity {
                         if(!studentID.getText().equals("") ){
                             infoDB.child("studentID").setValue(studentID.getText().toString());
                         }
+                        if(male.isChecked()) {
+                            infoDB.child("gender").setValue("Male");
+                        }
+                        if(female.isChecked()){
+                            infoDB.child("gender").setValue("Female");
+                        }
+                        if(Driver_box.isChecked()){
+                            infoDB.child("bikeID").setValue(BikeID.getText().toString());
+                            infoDB.child("brand").setValue(brand.getText().toString());
+                            infoDB.child("color").setValue(color.getText().toString());
+                        }
+                        if(Passenger_box.isChecked()){
+                            infoDB.child("bikeID").setValue("");
+                            infoDB.child("brand").setValue("");
+                            infoDB.child("color").setValue("");
+                        }
                     }
+
+
+
                 });
             }
 
@@ -91,25 +136,51 @@ public class editProfile extends AppCompatActivity {
             }
         });
 
-            male.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    if (compoundButton.isChecked()) {
-                        female.setChecked(false);
-                        infoDB.child("gender").setValue("male");
-                    }
+        male.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (compoundButton.isChecked()) {
+                    female.setChecked(false);
+
                 }
-            });
+            }
+        });
 
         female.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(compoundButton.isChecked()){
                     male.setChecked(false);
-                    infoDB.child("gender").setValue("Female");
+
                 }
             }
         });
+
+        Passenger_box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    Driver_box.setChecked(false);
+                    BikeID.setVisibility(View.INVISIBLE);
+                    brand.setVisibility(View.INVISIBLE);
+                    color.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        Driver_box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(compoundButton.isChecked()){
+                    Passenger_box.setChecked(false);
+                    BikeID.setVisibility(View.VISIBLE);
+                    brand.setVisibility(View.VISIBLE);
+                    color.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+
 
 
 
